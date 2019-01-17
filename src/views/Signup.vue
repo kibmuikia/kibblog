492f9691fd6929a4e2890aea6bd9da82c9ffa870
kibmuikia <muikiaa@gmail.com>
2019-01-17 14:02:42 -0500

    added some little functionality to the signUp form

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -6,10 +6,13 @@
 
 				<v-card>
 					<v-card-title class="">
-						<h5 class="headline" right>Sign In</h5>
+						<h5 class="headline" 
+							@click="$router.push('/signup?msg=refreshed')">
+							Sign Up
+						</h5>
 					</v-card-title>
 
-				<v-form ref="formSignUp">
+				<v-form ref="formSignUp" class="px-3">
 
 					<v-card-text>
 						<v-layout row wrap justify-space-between>
@@ -18,6 +21,7 @@
 									name="fname"
 									label="First Name"
 									id="fname"
+									v-model="fname"
 								></v-text-field>
 							</v-flex>
 							<v-flex xs12 md5>
@@ -25,6 +29,7 @@
 									name="lname"
 									label="Last Name"
 									id="lname"
+									v-model="lname"
 								></v-text-field>
 							</v-flex>
 						</v-layout>
@@ -32,11 +37,14 @@
 							name="username"
 							label="Username"
 							id="username"
+							v-model="username"
+							prepend-icon="person"
 						></v-text-field>
 						<v-text-field
 							name="email"
 							label="E-mail Address"
 							id="email"
+							v-model="email"
 						></v-text-field>
 					</v-card-text>
 
@@ -64,13 +72,32 @@
 <script>
 	
 export default {
+	data(){
+		return {
+			fname: '',
+			lname: '',
+			username: '',
+			email: '',
+			statusMsg: '',
+		}
+	},// END-data()
 	methods: {
 		reset() {
 			this.$refs.formSignUp.reset()
 		},// END-reset()
 		submitRegDetails() {
 			if ( this.$refs.formSignUp.validate() ) {
-				alert( 'Valid, Submiting & Processing Next' );
+				this.statusMsg = 'Valid, Submiting & Processing Next';
+				const newuser = {
+					nameFirst: this.fname,
+					nameLast: this.lname,
+					nameUser: this.username,
+					email: this.email
+				}
+				console.log( newuser );
+				alert( this.statusMsg );
+
+				// ....
 			} else {
 				alert( 'Invalid Details' );
 			}
